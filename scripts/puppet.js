92f3b06e8fa611a5c5950c6c5f71a475bdc0a707
Brian <kimsbrian@vt.edu>
2019-04-06 17:22:54 -0400

    beta

--- scripts/puppet.js
+++ scripts/puppet.js
@@ -104,10 +104,8 @@ async function run(number) {
   await page.goto(url);
   console.log('WAITING')
 
-  const html = await page.evaluate(() => {
     let bodyHTML = await page.evaluate(() => document.body.innerHTML);
     console.log('body'+bodyHTML);
-  })
 
 
   if (await page.$('div.ugc-base:nth-child(2)') !== null) {
